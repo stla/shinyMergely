8bd73c7c41c3729e326ddde9fb5da46b9d02cabc
stla <laurent_step@yahoo.fr>
2020-08-27 20:37:14 +0200

    cleaning ui

--- inst/shinyApp/ui.R
+++ inst/shinyApp/ui.R
@@ -22,15 +22,15 @@ languages <- list(
 fluidPage(
   theme = shinytheme("cyborg"),
   tags$head(
-    tags$script(src="bootstrap-flash-alert.js"),
-    tags$link(rel="stylesheet", href="animate.css"),
-    tags$script(src="codemirror/lib/codemirror.js"),
-    tags$link(rel="stylesheet", media="all", href="codemirror/lib/codemirror.css"),
-    tags$script(src="codemirror/addon/dialog/dialog.js"),
-    tags$link(rel = "stylesheet", href="codemirror/addon/dialog/dialog.css"),
-    tags$script(src="codemirror/addon/search/searchcursor.js"),
-    tags$script(src="codemirror/addon/search/search.js"),
-    tags$link(rel="stylesheet", href="codemirror/theme/cobalt.css"),
+    tags$script(src = "bootstrap-flash-alert.js"),
+    tags$link(rel="stylesheet", href = "animate.css"),
+    tags$script(src = "codemirror/lib/codemirror.js"),
+    tags$link(rel="stylesheet", media = "all", href = "codemirror/lib/codemirror.css"),
+    tags$script(src = "codemirror/addon/dialog/dialog.js"),
+    tags$link(rel = "stylesheet", href = "codemirror/addon/dialog/dialog.css"),
+    tags$script(src = "codemirror/addon/search/searchcursor.js"),
+    tags$script(src = "codemirror/addon/search/search.js"),
+    tags$link(rel="stylesheet", href = "codemirror/theme/cobalt.css"),
     tags$script(src = "codemirror/mode/meta.js"),
     tags$script(src = "codemirror/mode/css/css.js"),
     tags$script(src = "codemirror/mode/haskell/haskell.js"),
@@ -45,11 +45,11 @@ fluidPage(
     tags$script(src = "codemirror/mode/sql/sql.js"),
     tags$script(src = "codemirror/mode/stex/stex.js"),
     tags$script(src = "codemirror/mode/xml/xml.js"),
-    tags$script(type="text/javascript", src="mergely/package/lib/mergely.js"),
-    tags$link(rel="stylesheet", media="all", href="mergely/package/lib/mergely.css"),
-    tags$link(rel="stylesheet", href="shinyMergely.css"),
+    tags$script(src = "mergely/package/lib/mergely.js"),
+    tags$link(rel="stylesheet", media = "all", href = "mergely/package/lib/mergely.css"),
+    tags$link(rel="stylesheet", href = "shinyMergely.css"),
     tags$script(src = "shinyMergely.js"),
-    tags$link(rel="stylesheet", href="shootingStars.css")
+    tags$link(rel="stylesheet", href = "shootingStars.css")
     #tags$script(src = "https://cdn.jsdelivr.net/watch-element-resize.js/latest/watch-element-resize.min.js")
   ),
   br(),
@@ -64,11 +64,8 @@ fluidPage(
       ),
       column(
         width = 2,
-        #conditionalPanel(
-        #"output.uploaded",
         actionButton("swap", "Swap", class = "btn-lg mainPanel",
                      icon = icon("transfer", lib = "glyphicon"))
-        #)
       )
     ),
     fluidRow(
@@ -96,15 +93,11 @@ fluidPage(
       fluidRow(
         column(
           width = 6,
-          #style = "margin-left: -2px;",
           tags$p(id = "fileLeft")
-          #actionButton("copyLeft", "Copy")
         ),
         column(
           width = 6,
-          #          style = "margin-left: -13px;",
           tags$p(id = "fileRight")
-          #actionButton("copyRight", "Copy")
         )
       ),
       fluidRow(
@@ -129,38 +122,19 @@ fluidPage(
           actionButton("copyRight", "Copy")
         )
       ),
-      #conditionalPanel(
-      # "output.uploaded",
       jqui_resizable(
         tags$div(id="x",tags$div(id = "mergely")),
         options = list(alsoResize = ".CodeMirror", handles = "s")
       )
-      #)
     )
   ),
 
-  tags$div(class="sky", HTML(
-    '
-<div class="night">
-  <div class="shooting_star"></div>
-  <div class="shooting_star"></div>
-  <div class="shooting_star"></div>
-  <div class="shooting_star"></div>
-  <div class="shooting_star"></div>
-  <div class="shooting_star"></div>
-  <div class="shooting_star"></div>
-  <div class="shooting_star"></div>
-  <div class="shooting_star"></div>
-  <div class="shooting_star"></div>
-  <div class="shooting_star"></div>
-  <div class="shooting_star"></div>
-  <div class="shooting_star"></div>
-  <div class="shooting_star"></div>
-  <div class="shooting_star"></div>
-  <div class="shooting_star"></div>
-  <div class="shooting_star"></div>
-  <div class="shooting_star"></div>
-  <div class="shooting_star"></div>
-  <div class="shooting_star"></div>
-</div>'))
+  tags$div(
+    class = "sky",
+    tags$div(
+      class = "night",
+      do.call(tagList, rep(list(tags$div(class = "shooting_star")), 20L))
+    )
+  )
+
 )
